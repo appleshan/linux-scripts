04130ff7b62f052cae20fdf70c3abf7bd21dde30
appleshan <apple.shan@gmail.com>
2017-11-09 01:55:21 +0800

    install linux shell scripts

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# -*- shell-scripts -*-
+
+# ~/projects/linux-scripts/emacs/startup/ : emacs-connect / emacs-terminal / emacs-shutdown
+
+# emacs-connect
+ln -s ~/projects/linux-scripts/emacs/startup/emacs-connect ~/.local/bin/ec
+# emacs-terminal
+ln -s ~/projects/linux-scripts/emacs/startup/emacs-terminal ~/.local/bin/et
+# emacs-shutdown
+ln -s ~/projects/linux-scripts/emacs/startup/emacs-shutdown ~/.local/bin/es
